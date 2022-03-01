73210b90ad968909da0c15583992f8f58e99e632
Alin Voinea <contact@avoinea.com>
2022-03-01 19:07:11 +0200

    Enable multilingual

--- src/index.js
+++ src/index.js
@@ -1,4 +1,60 @@
 const applyConfig = (config) => {
+  if (process.env.NODE_ENV === 'production') {
+    // Restrict block-style to Layout only
+    config.settings.layoutOnlyBlockStyles = true;
+
+    // Restrict slate metadata mentions to Layout only
+    config.settings.layoutOnlySlateMetadataMentions = true;
+  } else {
+    // Enable description block for cypress
+    config.blocks.blocksConfig.description.restricted = false;
+  }
+
+  // Disable tags on View
+  config.settings.showTags = false;
+
+  // Enable Title block
+  config.blocks.blocksConfig.title.restricted = false;
+  config.blocks.requiredBlocks = [];
+
+  // Date format for EU
+  config.settings.dateLocale = 'en-gb';
+
+  // Multi-lingual
+  config.settings.isMultilingual = true;
+  config.settings.defaultLanguage = 'en';
+  config.settings.supportedLanguages = [
+    'en', // English
+    'sq', // Albanian
+    'bg', // Bulgarian
+    'bs', // Bosnian
+    'cs', // Czech
+    'hr', // Croatian
+    'da', // Danish
+    'nl', // Dutch
+    'el', // Greek
+    'et', // Estonian
+    'fi', // Finnish
+    'fr', // French
+    'de', // German
+    'hu', // Hungarian
+    'is', // Icelandic
+    'it', // Italian
+    'lv', // Latvian
+    'lt', // Lithuanian
+    'mk', // Macedonian
+    'mt', // Maltese
+    'no', // Norwegian
+    'pl', // Polish
+    'pt', // Portuguese
+    'ro', // Romanian
+    'sh', // Serbian
+    'sk', // Slovak
+    'sl', // Slovenian
+    'es', // Spanish
+    'sv', // Swedish
+    'tr', // Turkish
+  ];
   return config;
 };
 
