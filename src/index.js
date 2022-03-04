b3d6534a57f7288899c4f59b5cec553370363d8b
Alin Voinea <contact@avoinea.com>
2022-03-04 12:25:49 +0200

    Enable description block by default

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 const applyConfig = (config) => {
-  if (process.env.NODE_ENV === 'production') {
-    // Restrict block-style to Layout only
-    // config.settings.layoutOnlyBlockStyles = true;
-    // Restrict slate metadata mentions to Layout only
-    // config.settings.layoutOnlySlateMetadataMentions = true;
-  } else {
-    // Enable description block for cypress
-    config.blocks.blocksConfig.description.restricted = false;
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   // Restrict block-style to Layout only
+  //   config.settings.layoutOnlyBlockStyles = true;
+  //   // Restrict slate metadata mentions to Layout only
+  //   config.settings.layoutOnlySlateMetadataMentions = true;
+  // }
 
   // Disable tags on View
   config.settings.showTags = false;
 
   // Enable Title block
   config.blocks.blocksConfig.title.restricted = false;
+  // Enable description block (also for cypress)
+  config.blocks.blocksConfig.description.restricted = false;
   config.blocks.requiredBlocks = [];
 
   // Date format for EU
   config.settings.dateLocale = 'en-gb';
 
-  // Working-copy
+  // TODO: Working-copy
   // config.settings.hasWorkingCopySupport = true;
 
-  // Multi-lingual
+  // TODO: Multi-lingual
   // config.settings.isMultilingual = true;
   // config.settings.defaultLanguage = 'en';
   // config.settings.supportedLanguages = [
