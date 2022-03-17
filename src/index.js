a4e87ada5291307332c0c41121b17b60d1263d13
Alexandru Ghica <alec.ghica@eaudeweb.ro>
2022-03-17 13:45:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const applyConfig = (config) => {
   config.settings.dateLocale = 'en-gb';
 
   // #137187 Keycloak integration
-   if (window.env.KEYCLOAK_ENABLED === 'Yes') {
+   if (process.env.RAZZLE_KEYCLOAK === 'Yes') {
     config.settings.externalRoutes = [
       ...(config.settings.externalRoutes || []),
       {
