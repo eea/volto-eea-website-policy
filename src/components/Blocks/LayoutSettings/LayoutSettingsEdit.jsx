b330fcf86ba86b18e51704cd552eba40ae35ad97
ichim-david <ichim.david@gmail.com>
2022-11-16 18:22:35 +0200

    change(website-policy): added layout settings blocks to policy package (#24)
    * change(website-policy): added layout settings blocks to policy package
    
    - With initial support for adding layout width classes on the body
    
    * change(website-policy): memoize result of retrieval of styles from the layout settings
    
    - this way the getVoltoStyles function is called 2 times instead of 6 times
    
    * change(website-policy): memoize entire LayoutSettingsView
    
    * change(website-policy): use absolute path imports for layout views
    
    - this way they can be shadowed and continue to work without needing any
      extra import changes
    - export actions map so that they can be imported and customized if desired
    
    * change(layout-block): allow adding of block only on layout control panel
    
    - This allows us to eventually hide it for users leaving only the config
      in the layout page
    
    * change(layout-block): hide block on edit and add page
    
    - instead of not adding any content on edit or add simply hide it,
      this way in case we ever need to perform any changes we can disable from
      devtools the hiding and interact with the block settings
    
    * [JENKINS] - Remove broken eslint.rc
    
    Co-authored-by: Alin Voinea <contact@avoinea.com>

---
+++ src/components/Blocks/LayoutSettings/LayoutSettingsEdit.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { EditSchema } from '@eeacms/volto-eea-website-policy/components/Blocks/LayoutSettings/schema';
+import { BlockDataForm, SidebarPortal } from '@plone/volto/components';
+import LayoutSettingsView from '@eeacms/volto-eea-website-policy/components/Blocks/LayoutSettings/LayoutSettingsView';
+import '@eeacms/volto-eea-website-policy/components/Blocks/LayoutSettings/edit.less';
+
+const LayoutSettingsEdit = (props) => {
+  const schema = EditSchema();
+  return (
+    <>
+      <h3>Page layout settings</h3>
+      <LayoutSettingsView {...props} />
+      <SidebarPortal selected={props.selected}>
+        {props.selected && (
+          <BlockDataForm
+            title={schema.title}
+            schema={schema}
+            formData={props.data}
+            onChangeField={(id, value) => {
+              props.onChangeBlock(props.block, {
+                ...props.data,
+                [id]: value,
+              });
+            }}
+          />
+        )}
+      </SidebarPortal>
+    </>
+  );
+};
+
+export default LayoutSettingsEdit;
