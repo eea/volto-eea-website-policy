b4623f066a2212ec60725dc16326eae9f0170a3b
Miu Razvan <miu.razvan28@gmail.com>
2023-07-28 12:18:01 +0300

    chore: set DataProvenace based on behavior

--- src/components/Widgets/DataProvenance.jsx
+++ src/components/Widgets/DataProvenance.jsx
@@ -1,6 +1,12 @@
-// import React from 'react';
-// import RichTextWidget from '@plone/volto-slate/widgets/RichTextWidget';
+import RichTextWidget from '@plone/volto-slate/widgets/RichTextWidget';
+import { DataProvenance as NextDataProvenance } from '@eeacms/volto-widget-dataprovenance/components/theme/Widgets/DataProvenance';
 
-export const DataProvenance = () =>
-  // <RichTextWidget {...props} readOnly={true} />
-  ' Data Provenance ';
+export const DataProvenance = (props) => {
+  if (props.behavior === 'eea.coremetadata.behavior') {
+    return <NextDataProvenance {...props} />;
+  }
+  if (props.behavior === 'eea.dexterity.indicators.metadata') {
+    return <RichTextWidget {...props} />;
+  }
+  return ' Data Provenance ';
+};
