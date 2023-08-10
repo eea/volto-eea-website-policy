7fc7c789f4a21e8040bd40bbda981fe03a78c95b
Alin Voinea <contact@avoinea.com>
2023-08-10 20:51:23 +0300

    refactor: Remove institutional_mandate widget - refs #256516 (#60)

--- src/components/Widgets/InstitutionalMandate.jsx
+++
@@ -1,5 +0,0 @@
-import RichTextWidget from '@plone/volto-slate/widgets/RichTextWidget';
-
-export const InstitutionalMandate = (props) => {
-  return <RichTextWidget {...props} readOnly={true} />;
-};
