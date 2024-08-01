c6ac4937519d41f631757472f49f0b609ed8cda1
nileshgulia1 <nileshgulia@gmail.com>
2024-08-01 14:56:53 +0530

    fix: behave as links when no childs are present

--- src/components/Blocks/ContextNavigation/variations/Accordion.jsx
+++ src/components/Blocks/ContextNavigation/variations/Accordion.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Accordion } from 'semantic-ui-react';
+import cx from 'classnames';
 import { Link } from 'react-router-dom';
 import { compose } from 'redux';
 import { withRouter } from 'react-router';
 import { defineMessages, useIntl } from 'react-intl';
 
 import { flattenToAppURL } from '@plone/volto/helpers';
-import { Icon, Image } from '@plone/volto/components';
+import { Icon, Image, UniversalLink } from '@plone/volto/components';
 import { withContentNavigation } from '@plone/volto/components/theme/Navigation/withContentNavigation';
 
 import upIcon from '@plone/volto/icons/up-key.svg';
@@ -34,7 +35,9 @@ function renderItems({
     thumb,
     is_current,
     type,
+    description,
     normalized_id,
+    is_in_path,
     items: childItems,
     ...rest
   } = item;
@@ -43,16 +46,46 @@ function renderItems({
   const hasChildItems = childItems && childItems.length > 0;
   return (
     <React.Fragment key={index}>
-      <Accordion.Title
-        active={isActive}
-        onClick={() => handleTitleClick(normalized_id, hasChildItems)}
-      >
-        {hasChildItems && (
-          <Icon size="24px" name={isActive ? upIcon : rightIcon} />
-        )}
-        {thumb ? <Image src={flattenToAppURL(thumb)} /> : ''}
-        <p>{title}</p>
-      </Accordion.Title>
+      {hasChildItems ? (
+        <Accordion.Title
+          active={isActive}
+          onClick={() => handleTitleClick(normalized_id, hasChildItems)}
+        >
+          {hasChildItems && (
+            <Icon size="24px" name={isActive ? upIcon : rightIcon} />
+          )}
+          {thumb ? <Image src={flattenToAppURL(thumb)} /> : ''}
+          <p>{title}</p>
+        </Accordion.Title>
+      ) : (
+        <div className="title">
+          <p>
+            {type !== 'link' ? (
+              <Link
+                to={flattenToAppURL(href)}
+                title={description}
+                className={cx(`contenttype-${type}`, {
+                  in_path: is_in_path,
+                })}
+              >
+                {thumb ? <Image src={flattenToAppURL(thumb)} /> : ''}
+                {title}
+                {is_current ? (
+                  <div className="active-indicator">
+                    <Icon name={rightIcon} size="30px" />
+                  </div>
+                ) : (
+                  ''
+                )}
+              </Link>
+            ) : (
+              <UniversalLink href={flattenToAppURL(href)}>
+                {title}
+              </UniversalLink>
+            )}
+          </p>
+        </div>
+      )}
       <Accordion.Content active={isActive}>
         {hasChildItems && (
           <Accordion className="default">
