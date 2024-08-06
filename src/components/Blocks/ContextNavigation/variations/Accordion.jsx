9c73cd55b5303df958c64a12a251f9a9b15d35ef
David Ichim <ichim.david@gmail.com>
2024-08-06 20:03:30 +0300

    change(accordion): simplify logic and improve render by have a list of elements instead of div tags
    - Also we only have accordions where we have child data the rest is simply list of links

--- src/components/Blocks/ContextNavigation/variations/Accordion.jsx
+++ src/components/Blocks/ContextNavigation/variations/Accordion.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Accordion } from 'semantic-ui-react';
 import cx from 'classnames';
-import { Link } from 'react-router-dom';
 import { compose } from 'redux';
 import { withRouter } from 'react-router';
 import { defineMessages, useIntl } from 'react-intl';
 
 import { flattenToAppURL } from '@plone/volto/helpers';
-import { Icon, Image, UniversalLink } from '@plone/volto/components';
+import { Icon, UniversalLink } from '@plone/volto/components';
 import { withContentNavigation } from '@plone/volto/components/theme/Navigation/withContentNavigation';
 
 import upIcon from '@plone/volto/icons/up-key.svg';
-import rightIcon from '@plone/volto/icons/right-key.svg';
 import downIcon from '@plone/volto/icons/down-key.svg';
 
 const messages = defineMessages({
@@ -22,208 +20,99 @@ const messages = defineMessages({
   },
 });
 
-function renderItems({
-  item,
-  index,
-  activeItems,
-  handleTitleClick,
-  parentLevel,
-}) {
-  const level = parentLevel + 1;
-  const {
-    title,
-    href,
-    thumb,
-    is_current,
-    type,
-    description,
-    normalized_id,
-    is_in_path,
-    items: childItems,
-  } = item;
-
-  const isActive = is_current || !!activeItems[normalized_id];
-  const hasChildItems = childItems && childItems.length > 0;
-  return (
-    <React.Fragment key={index}>
-      {hasChildItems ? (
-        <Accordion.Title
-          active={isActive}
-          index={index}
-          onKeyDown={(e) => {
-            if (e.keyCode === 13 || e.keyCode === 32) {
-              e.preventDefault();
-              handleTitleClick(normalized_id, hasChildItems);
-            }
-          }}
-          role="button"
-          tabIndex={0}
-          aria-expanded={isActive}
-          onClick={() => handleTitleClick(normalized_id, hasChildItems)}
-        >
-          {hasChildItems && (
-            <Icon size="24px" name={isActive ? upIcon : rightIcon} />
-          )}
-          {thumb ? <Image src={flattenToAppURL(thumb)} /> : ''}
-          <p>{title}</p>
-        </Accordion.Title>
-      ) : (
-        <div
-          className={cx('title', {
-            active: is_current,
-          })}
-        >
-          <p>
-            {type !== 'link' ? (
-              <Link
-                to={flattenToAppURL(href)}
-                title={description}
-                className={cx(`contenttype-${type}`, {
-                  in_path: is_in_path,
-                })}
-              >
-                {thumb ? <Image src={flattenToAppURL(thumb)} /> : ''}
-                {title}
-              </Link>
-            ) : (
-              <UniversalLink href={flattenToAppURL(href)}>
-                {title}
-              </UniversalLink>
-            )}
-          </p>
-        </div>
-      )}
-      {hasChildItems ? (
-        <Accordion.Content active={isActive}>
-          {hasChildItems && (
-            <Accordion className="default" key={index} id={index}>
-              {childItems.map((item, index) =>
-                renderItems({
-                  item,
-                  index,
-                  activeItems,
-                  handleTitleClick,
-                  parentLevel: level,
-                }),
-              )}
-            </Accordion>
-          )}
-        </Accordion.Content>
-      ) : (
-        ''
-      )}
-    </React.Fragment>
-  );
-}
-
-const findTopLevelParentWithCurrent = (items) => {
-  const inPathItems = [];
-  const computeActive = (items) => {
-    items.forEach((item) => {
-      if (item.is_in_path) inPathItems.push(item);
-      if (item.items && item.items.length > 0) computeActive(item.items);
-    });
-  };
-  computeActive(items || []);
-  return inPathItems;
-};
-
-const AccordionNavigation = (props) => {
-  const { navigation = {} } = props;
-  const { items = [] } = navigation;
+const AccordionNavigation = ({ navigation = {} }) => {
+  const { items = [], title, has_custom_name } = navigation;
   const intl = useIntl();
+  const [isNavOpen, setNavOpen] = React.useState(true);
   const [activeItems, setActiveItems] = React.useState({});
 
-  const [isNavOpen, setNavOpen] = React.useState(true);
-
-  const handleTitleClick = (index, hasChildItems) => {
-    if (hasChildItems) {
-      setActiveItems((prevActiveItems) => ({
-        ...prevActiveItems,
-        [index]: !prevActiveItems[index],
-      }));
-    }
-  };
-
-  React.useEffect(() => {
-    const parents = findTopLevelParentWithCurrent(items);
-    if (parents.length) {
-      const idMapping = parents
-        .filter(Boolean)
-        .reduce((acc, item) => ({ ...acc, [item?.normalized_id]: true }), {});
-
-      setActiveItems((prevActiveItems) => ({
-        ...prevActiveItems,
-        ...idMapping,
-      }));
-    }
-  }, [items]);
-
-  const onClickSummary = (e) => {
+  const onClickSummary = React.useCallback((e) => {
     e.preventDefault();
-    setNavOpen(!isNavOpen);
+    setNavOpen((prev) => !prev);
+  }, []);
+
+  const renderItems = ({ item, level = 0 }) => {
+    const {
+      title,
+      href,
+      is_current,
+      is_in_path,
+      items: childItems,
+      type,
+    } = item;
+    const hasChildItems = childItems && childItems.length > 0;
+
+    const checkIfActive = () => {
+      return activeItems[href] !== undefined ? activeItems[href] : is_in_path;
+    };
+
+    const isActive = checkIfActive();
+
+    const handleTitleClick = () => {
+      setActiveItems((prev) => ({ ...prev, [href]: !isActive }));
+    };
+
+    const handleKeyDown = (e) => {
+      if (e.keyCode === 13 || e.keyCode === 32) {
+        e.preventDefault();
+        handleTitleClick();
+      }
+    };
+
+    return (
+      <li className={cx({ title: true, is_in_path })} key={href}>
+        {hasChildItems ? (
+          <Accordion className="default">
+            <Accordion.Title
+              active={isActive}
+              as={'button'}
+              aria-expanded={isActive}
+              onClick={handleTitleClick}
+              onKeyDown={handleKeyDown}
+              className={cx({ active: is_in_path })}
+            >
+              <Icon name={isActive ? upIcon : downIcon} size="24px" />
+              <span className="title-text">{title}</span>
+            </Accordion.Title>
+            <Accordion.Content active={isActive}>
+              <ul>
+                {childItems.map((child) =>
+                  renderItems({ item: child, level: level + 1 }),
+                )}
+              </ul>
+            </Accordion.Content>
+          </Accordion>
+        ) : (
+          <UniversalLink
+            href={flattenToAppURL(href)}
+            className={cx(`contenttype-${type}`, {
+              current: is_current,
+              in_path: is_in_path,
+            })}
+          >
+            {title}
+          </UniversalLink>
+        )}
+      </li>
+    );
   };
 
   return items.length ? (
     <nav className="context-navigation">
       <details open={isNavOpen}>
-        {navigation.has_custom_name ? (
-          <summary
-            className="context-navigation-header"
-            onClick={onClickSummary}
-            role="button"
-            tabIndex={0}
-            onKeyDown={(e) => {
-              if (e.keyCode === 13 || e.keyCode === 32) {
-                e.preventDefault();
-                onClickSummary(e);
-              }
-            }}
-          >
-            {navigation.title}
-            <Icon
-              name={isNavOpen ? upIcon : downIcon}
-              size="40px"
-              style={{ marginLeft: 'auto' }}
-            />
-          </summary>
-        ) : (
-          <summary
-            className="context-navigation-header"
-            onClick={onClickSummary}
-            role="button"
-            tabIndex={0}
-            onKeyDown={(e) => {
-              if (e.keyCode === 13 || e.keyCode === 32) {
-                e.preventDefault();
-                onClickSummary(e);
-              }
-            }}
-          >
-            {intl.formatMessage(messages.navigation)}
-            <Icon
-              name={isNavOpen ? upIcon : downIcon}
-              size="40px"
-              style={{ marginLeft: 'auto' }}
-            />
-          </summary>
-        )}
-        <Accordion className="default">
-          {items.map((item, index) =>
-            renderItems({
-              item,
-              index,
-              activeItems,
-              handleTitleClick,
-              parentLevel: 0,
-            }),
-          )}
-        </Accordion>
+        {/* eslint-disable-next-line */}
+        <summary className="context-navigation-header" onClick={onClickSummary}>
+          {has_custom_name ? title : intl.formatMessage(messages.navigation)}
+          <Icon
+            name={isNavOpen ? upIcon : downIcon}
+            size="40px"
+            style={{ marginLeft: 'auto' }}
+          />
+        </summary>
+        <ul>{items.map((item, index) => renderItems({ item }))}</ul>
       </details>
     </nav>
-  ) : (
-    ''
-  );
+  ) : null;
 };
 
 AccordionNavigation.propTypes = {
