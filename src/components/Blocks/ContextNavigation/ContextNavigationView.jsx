7f5c122c596eb4db7f0ea4c81708b3c88aca657b
David Ichim <ichim.david@gmail.com>
2024-08-19 14:32:07 +0300

    dropped useless code

--- src/components/Blocks/ContextNavigation/ContextNavigationView.jsx
+++ src/components/Blocks/ContextNavigation/ContextNavigationView.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { injectIntl } from 'react-intl';
 import { flattenToAppURL, withBlockExtensions } from '@plone/volto/helpers';
 import DefaultTemplate from './variations/Default';
 
@@ -9,8 +8,7 @@ const ContextNavigationView = (props = {}) => {
   const root_path = data?.root_node?.[0]?.['@id'];
   if (root_path) navProps['root_path'] = flattenToAppURL(root_path);
   const Renderer = variation?.view ?? DefaultTemplate;
-  delete navProps.variation;
-  return Renderer ? <Renderer params={navProps} /> : '';
+  return <Renderer params={navProps} />;
 };
 
-export default injectIntl(withBlockExtensions(ContextNavigationView));
+export default withBlockExtensions(ContextNavigationView);
